74b59fa0cea1be7dff476131594118dc44322e1c
Shubh14042002 <shubh.agg12@gmail.com>
2024-05-13 05:31:47 -0600

    added links to repos of some projects
    added href links to the visit website component

--- components/Works/data/data.js
+++ components/Works/data/data.js
@@ -4,14 +4,16 @@ const data = [
         image_url: "/eventsphere.png",
         work_title: "Event Management Web App",
         description: "EventSphere is a web application designed to enhance campus engagement by allowing students to easily create, manage, and find local events. The platform features streamlined event setup, an efficient RSVP process, and an integrated notification system that confirms registrations and alerts attendees to event changes such as cancellations or deletions.",
-        genre: "React.js, Node.js, MongoDB, REST "
+        genre: "React.js, Node.js, MongoDB, REST ",
+        code_url: "https://github.com/Shubh14042002/EventSphere-main"
     },
     {
         id: 2,
         image_url: "/rcplane.png",
         work_title: "RC airplane",
         description: "A lightweight, printable RC aircraft that involved selecting the NACA 00012-63 airfoil for optimal lift , performing stability considerations by adjusting the tail volume coefficients and ensuring a proper Static Margin for longitudinal stability. Final design features include tailored control surfaces like ailerons, elevators, and rudders to enhance maneuverability.",
-        genre: "3D-Printing, AUTO-CAD, OpenVSP"
+        genre: "3D-Printing, AUTO-CAD, OpenVSP",
+        code_url:""
     },
     {
         
@@ -19,21 +21,24 @@ const data = [
         image_url: "/wildliferescue.png",
         work_title: "Wildlife Rescue Scheduler ",
         description: "The Wildlife Rescue Developer system automates scheduling for wildlife caretakers, encompassing fixed cage cleaning, feeding, and special treatment tasks like bandaging and administering medicine. It efficiently organizes daily operations and notifies when volunteer assistance is needed, streamlining care and ensuring timely attention for all animals.",
-        genre: "Java, GUI, MYSQL"
+        genre: "Java, GUI, MYSQL",
+        code_url:"https://github.com/Shubh14042002/WildlifeRescueScheduleDeveloper"
     },
     {
         id: 4,
         image_url: "/hospital.png",
         work_title: "Hospital Management ",
         description: "The Hospital Management System is a web-based platform that streamlines hospital operations, enabling efficient management of staff, patient appointments, and medical records. It supports user registration and provides quick access to medical references, improving both administrative tasks and patient care.",
-        genre: "HTML, CSS, Bootstrap, JavaScript, Node.js, MySQL"
+        genre: "HTML, CSS, Bootstrap, JavaScript, Node.js, MySQL",
+        code_url:"https://github.com/Shubh14042002/hospital_management_system"
     },
     {
         id: 5,
         image_url: "/rdc.png",
         work_title: "Software Reliability Assessment",
         description: "The team analyzed integration test data using tools such as C-SFRAT and RDC-11, employing reliability growth testing and reliability demonstration charts. We compared outcomes from both methods, highlighting their similarities and differences to enhance their understanding of testing reliability.",
-        genre: "C-SFRAT, SRTAT, RDC"
+        genre: "C-SFRAT, SRTAT, RDC",
+        code_url:""
     }
     // },
     // {
